78a3026169d1ef91d7c3d624cf828fed4c1bbb4d
EduardoFORodrigues <dudacriativo@hotmail.com>
2024-11-06 01:11:37 -0300

    feat:including env editing

--- APIRESTComNODEJS/src/config/database.js
+++ APIRESTComNODEJS/src/config/database.js
@@ -1,5 +1,8 @@
 // CONFIGURANDO AMBIENTE
 let ambiente = undefined;
+switch(process.env.PUBLICAR){
+    case "HTML": ambiente = configurarHML();
+}
 
 
 //ambiente de Homolocação
@@ -12,6 +15,10 @@ function configurardoHML(){
         username:process.env.HTML_USER_NAME,
         password:process.env.HTML_PASSWORD,
         database:process.env.Html_DATABASE,
+        define:{
+            timestamps:true,
+            undercored:true,
+        }
         
     
     }
@@ -19,6 +26,20 @@ function configurardoHML(){
 
 // ambiente de Produtção
 function configurarPROD(){
+    return{
+        dialect:process.env.PROD_DIALECT,
+        host:process.env.PROD_HOST,
+        port:process.env.PROD_PORT,
+        username:process.env.PROD_USER_NAME,
+        password:process.env.PROD_PASSWORD,
+        database:process.env.PROD_DATABASE,
+        define:{
+            timestamps:true,
+            undercored:true,
+        }
+        
+    
+    }
 
 }
 
@@ -26,4 +47,21 @@ function configurarPROD(){
 
 function configurarLOCAL(){
 
+    return{
+        dialect:process.env.LOCAL_DIALECT,
+        host:process.env.LOCAL_HOST,
+        port:process.env.LOCAL_PORT,
+        username:process.env.LOCAL_USER_NAME,
+        password:process.env.LOCAL_PASSWORD,
+        database:process.env.LOCAL_DATABASE,
+        define:{
+            timestamps:true,
+            undercored:true,
+        }
+        
+    
+    }
+
 }
+
+module.exports = ambiente;
