3a9b0e8d8102c202feabc3bf855253aed5b60bd4
EduardoFORodrigues <dudacriativo@hotmail.com>
2024-11-14 10:16:25 -0300

    feat:add migrations

--- APIRESTComNODEJS/src/config/database.js
+++ APIRESTComNODEJS/src/config/database.js
@@ -1,36 +1,26 @@
 //const { underscoredIf } = require("sequelize/lib/utils");
 let ambiente = undefined;
-switch(process.env.PUBLICAR){
-<<<<<<< HEAD
 
-    case "HML" : ambiente = configurarHML();
-        break;     
-        case "PROD" : ambiente = configurarPROD();
-    break;     
-        default: 
+switch(process.env.PUBLICAR){
+
+case "HML" : 
+      ambiente = configurarHML();
+      break;  
+case "PROD" : 
+      ambiente = configurarPROD();
+      break;     
+default: 
         ambiente = configurarLOCAL();
    
-}
-
-// CONFIGURANDO AMBIENTE
-
-=======
-    case "HTML": 
-    ambiente = configurarHML();
-    break;
-    case 'PROD':
-        ambiente = configurarPROD();
-    break;
-}
->>>>>>> origin
-
+}   
+   
 
 //ambiente de Homolocação
 function configurardoHML(){
 
     return{
+
         dialect:process.env.HML_DIALECT,
-<<<<<<< HEAD
         host:process.env.HML_HOST,
         port:process.env.HML_PORT,
         username:process.env.HML_USER_NAME,
@@ -40,25 +30,15 @@ function configurardoHML(){
             timestamps:true,
             underscored:true,
         }
-=======
-        host:process.env.HTML_HOST,
-        port:process.env.HTML_PORT,
-        username:process.env.HTML_USER_NAME,
-        password:process.env.HTML_PASSWORD,
-        database:process.env.Html_DATABASE,
-        define:{
-            timestamps:true,
-            undercored:true,
-        }
-        
->>>>>>> origin
-    
+     
     }
 }
 
 // ambiente de Produtção
 function configurarPROD(){
     return{
+
+
         dialect:process.env.PROD_DIALECT,
         host:process.env.PROD_HOST,
         port:process.env.PROD_PORT,
@@ -66,22 +46,14 @@ function configurarPROD(){
         password:process.env.PROD_PASSWORD,
         database:process.env.PROD_DATABASE,
         define:{
-            timestamps:true,
-<<<<<<< HEAD
+            timestamps:true,  
             underscored:true,
         }
     
     }
-=======
-            undercored:true,
-        }
-        
-    
-    }
-
->>>>>>> origin
 }
-
+    
+    
 
 
 //mbiente de  Local
@@ -89,6 +61,8 @@ function configurarPROD(){
 function configurarLOCAL(){
 
     return{
+
+        
         dialect:process.env.LOCAL_DIALECT,
         host:process.env.LOCAL_HOST,
         port:process.env.LOCAL_PORT,
@@ -97,20 +71,11 @@ function configurarLOCAL(){
         database:process.env.LOCAL_DATABASE,
         define:{
             timestamps:true,
-<<<<<<< HEAD
+
             underscored:true,
         }
 
- }
-}
-=======
-            undercored:true,
-        }
-        
-    
     }
-
 }
 
->>>>>>> origin
-module.exports = ambiente;
+     
